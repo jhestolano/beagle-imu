6afeba666aab9b005a20e85805792f98408ce896
jhestolano <jhestolano@gmail.com>
2024-07-28 20:45:48 +0000

    Compile mongoose in folder to avoid re-compiling all the time.

--- beagle/build.sh
+++ beagle/build.sh
@@ -9,11 +9,18 @@ echo "building mpu9250 demo..."
 gcc -Wall -Werror -Wpedantic -c mpu9250.c
 gcc -Wall -Werror -Wpedantic -c test_mpu9250.c
 mv *.o ./build
-gcc ./build/mpu9250.o ./build/test_mpu9250.o -o mpu9250_test
+gcc ./build/mpu9250.o ./build/test_mpu9250.o -o ./build/mpu9250_test
 echo "done."
 
-echo "building websockets app..."
-gcc -Wall -Werror -Wpedantic -c server.c
-mv *.o ./build
-gcc ./build/mpu9250.o ./build/server.o -o mpu9250_ws
+if [ ! -f ../mongoose/mongoose.o ]; then
+	echo "building mongoose..."
+	cd ../mongoose/
+	gcc -Wall -Werror -Wpedantic -c mongoose.c
+	echo "done."
+fi
+
+echo "building app..."
+gcc -Wall -Werror -Wpedantic -c -I../mongoose/ server.c
+mv *.o build/
+gcc ./build/mpu9250.o ./build/server.o ../mongoose/mongoose.o -o ./build/mpu9250_ws
 echo "done."
