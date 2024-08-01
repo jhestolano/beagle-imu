0c7435f3cf25ccf4664b1d56ade17a3646ff03ee
jhestolano <jhestolano@gmail.com>
2024-08-01 06:59:56 +0000

    Got timer working.

--- beagle/server.c
+++ beagle/server.c
@@ -2,54 +2,73 @@
 #include "mpu9250.h"
 #include <stdio.h>
 
-static const char *s_listen_on = "ws://0.0.0.0:8000";
-static const char *s_web_root = ".";
+static const char *gs_listen_on = "ws://0.0.0.0:8000";
+
+struct websock_data {
+  struct mg_mgr mgr;
+  struct mg_timer imu_timer;
+  struct mpu9250 *imu_dev;
+};
+
+static void tm_fn(void *data) {
+  static int cnt = 0;
+  struct websock_data *ws_data = (struct websock_data *)data;
+  (void)ws_data;
+  puts("triggered!");
+  if (cnt++ > 20) {
+    mg_timer_free(&ws_data->mgr.timers, &ws_data->imu_timer);
+    cnt = 0;
+    puts("done!");
+  }
+}
 
 static void ws_fn(struct mg_connection *conn, int ev, void *ev_data) {
+  struct websock_data *ws_data = (struct websock_data *)conn->fn_data;
+  (void)ws_data;
+
   if (ev == MG_EV_OPEN) {
     puts("Connection started!");
-
   } else if (ev == MG_EV_HTTP_MSG) {
     struct mg_http_message *hm = (struct mg_http_message *)ev_data;
-    if (mg_match(hm->uri, mg_str("/websocket"), NULL)) {
+    if (mg_match(hm->uri, mg_str("/"), NULL)) {
       mg_ws_upgrade(conn, hm, NULL);
-    } else if (mg_match(hm->uri, mg_str("/imu"), NULL)) {
-      struct mpu9250 *dev = conn->fn_data;
-      /* Get sensor data. */
-      struct vec3f accel;
-      (void)mpu9250_get_accel(dev, &accel);
-      mg_http_reply(conn, 200, "", "{\"accel_z\": %f}\n", accel.z);
-    } else {
-      struct mg_http_serve_opts opts = {.root_dir = s_web_root};
-      mg_http_serve_dir(conn, ev_data, &opts);
+      puts("Connection upgraded to websockets");
+      mg_timer_init(&ws_data->mgr.timers, &ws_data->imu_timer, 100u,
+                    MG_TIMER_REPEAT, tm_fn, ws_data);
     }
-
   } else if (ev == MG_EV_WS_MSG) {
     // Got websocket frame. Received data is wm->data. Echo it back!
     struct mg_ws_message *msg = (struct mg_ws_message *)ev_data;
     printf("Got message: %.*s\n\r", (int)msg->data.len, msg->data.buf);
     mg_ws_send(conn, msg->data.buf, msg->data.len, WEBSOCKET_OP_TEXT);
+  } else if (ev == MG_EV_CLOSE) {
+    // mg_timer_free(&ws_data->mgr.timers, &ws_data->imu_timer);
+    puts("Connection closed!");
   } else {
     /* Does nothing. */
   }
 }
 
 int main(void) {
+  struct websock_data ws_data = {
+      .imu_dev = mpu9250_init(MPU9250_DEFAULT_CONFIG),
+  };
 
-  struct mpu9250 *dev = mpu9250_init(MPU9250_DEFAULT_CONFIG);
-
-  if (!dev) {
+  if (!ws_data.imu_dev) {
     /* No point to continue. Just die. */
     return 1;
   }
 
-  struct mg_mgr mgr; // Event manager
-  mg_mgr_init(&mgr); // Initialise event manager
-  printf("Starting WS listener on %s/websocket\n", s_listen_on);
-  mg_http_listen(&mgr, s_listen_on, ws_fn, (void *)dev); // Create HTTP listener
+  mg_mgr_init(&ws_data.mgr);
+
+  printf("Starting WS listener on %s/websocket\n", gs_listen_on);
+
+  mg_http_listen(&ws_data.mgr, gs_listen_on, ws_fn, (void *)&ws_data);
   for (;;)
-    mg_mgr_poll(&mgr, 1000); // Infinite event loop
-  mg_mgr_free(&mgr);
-  mpu9250_deinit(dev);
+    mg_mgr_poll(&ws_data.mgr, 10u);
+
+  mg_mgr_free(&ws_data.mgr);
+  mpu9250_deinit(ws_data.imu_dev);
+
   return 0;
 }
